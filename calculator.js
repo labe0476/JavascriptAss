63b2cf57ea2b190c9b78b1201113f943adb075d9
Labe <jimohidrisalabelewe@gmail.com>
2025-10-12 21:24:27 +0100

    labe

---
+++ calculator.js
@@ -0,0 +1,12 @@
+var output = document.getElementById("output");
+function addInput(x) {
+  output.value += x;
+}
+
+function equalto() {
+  output.value = eval(output.value);
+}
+
+function clear() {
+  output.value = '';
+}
